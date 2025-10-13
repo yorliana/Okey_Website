187059064b0a4fb73ddc7ef5093d015c7e8575b1
yorliana <ing.yorlianam24@gmail.com>
2025-10-13 18:10:51 +0200

    Update Carrusel.jsx
    Logo Neew

--- src/Pages/Carrusel/Carrusel.jsx
+++ src/Pages/Carrusel/Carrusel.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import image1 from '../../assets/img/Logookey.png';
+import image1 from '../../assets/img/Logokey.png';
 import image2 from '../../assets/img/Logookey2.png';
 import image3 from '../../assets/img/Logookey.png';
 import style from './Carrusel.module.css'
 
 import Carosuel from '../../components/SliderBanner/Carosuel';
+
 const Carrusel = () => {
   const images = [image1, image2, image3];
 
