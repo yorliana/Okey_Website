1c383d81c9c288f5a0b669c17f2cd7d006a777dc
yorliana <ing.yorlianam24@gmail.com>
2026-01-03 15:43:54 +0100

    actualizacion mgt

--- src/components/EasyMgt/EasyMgt.jsx
+++ src/components/EasyMgt/EasyMgt.jsx
@@ -1,13 +1,13 @@
  
 import React from "react";
-import style from  './EasyMgt.module.css'
+import styles from  './EasyMgt.module.css'
 import {Link} from "react-router-dom";
 import img from "../../assets/img/okeylogo.png"
 import {useLocation} from "react-router-dom"
 import botoflecha from "../../assets/img/flechanegra.png"
 import boton from "../../assets/img/blotoflecha.png"
 import img1 from "../../assets/img/PORTADA.jpg"
-import miVideo from "../../../public/Easy-MGT.mp4"
+import video from "../../assets/video/video.mp4"
 import RedesEasy from "../EasyMgt/RedesEasy/RedesEasy";
 const EasyMgt = () => {
  
@@ -19,25 +19,25 @@ const EasyMgt = () => {
 
   return (
 
-    <><div className={style.contenedor}>
+    <><div className={styles.contenedor}>
 
      
-   <img className={style.img} src={img} alt="" />
+   <img className={styles.img} src={img} alt="" />
 
-      <div className={style.flecha}>
-        <a className={style.btn} href="/"><img className={style.boto} src={boton} alt="" /></a>
-
-      </div>
-      <div className={style.letraspro}>
-        <p className={style.p}>Motion</p>
+      <div className={styles.flecha}>
+        <a className={styles.btn} href="/"><img className={styles.boto} src={boton} alt="" /></a>
 
       </div>
-      <div className={style.morado}>
-        <span className={style.flechabajo}>↓</span>
+      <div className={styles.letraspro}>
+        <p className={styles.p}>Motion</p>
+
+      </div>
+      <div className={styles.morado}>
+        <span className={styles.flechabajo}>↓</span>
        
       </div>
-      <div className={style.easy}>
- <h2 className={style.h2}>EASY MGT</h2>
+      <div className={styles.easy}>
+ <h2 className={styles.h2}>EASY MGT</h2>
       </div>
 
 
@@ -49,14 +49,14 @@ const EasyMgt = () => {
       <div>
 </div>
 
-<div className={style.section}>
+<div className={styles.section}>
 
-  <div className={style.description}>
-    <div className={style.lorem}>
+  <div className={styles.description}>
+    <div className={styles.lorem}>
 
     
-    <h1 className={style.H1}>DESCRIPCION DEL PROYECTO</h1>
-    <p className={style.lorems}>Easy-MGT es un ecosistema inmobiliario y comunitario 
+    <h1 className={styles.H1}>DESCRIPCION DEL PROYECTO</h1>
+    <p className={styles.lorems}>Easy-MGT es un ecosistema inmobiliario y comunitario 
       en una plataforma inteligente. Dirigido a personas que gestionan o venden propiedades,
        que se encargan del mantenimiento de viviendas o simplemente para arrendar. 
        Dicho esto, nuestro cliente necesitaba desarrollar una video explicativo, fácil 
@@ -66,50 +66,44 @@ const EasyMgt = () => {
 
       
     </div>
-    <div className={style.vertical}  style={{  width: 0, height: 250, border: '0.10px black solid'}}></div>
+    <div className={styles.vertical}  style={{  width: 0, height: 250, border: '0.10px black solid'}}></div>
 
-  <div className={style.lorem1}>
-    <h1 className={style.H1}>SE HIZO</h1>
-    <p  className={style.lorems}>Diseño de story board<br />
+  <div className={styles.lorem1}>
+    <h1 className={styles.H1}>SE HIZO</h1>
+    <p  className={styles.lorems}>Diseño de story board<br />
       Video motion graphics a <br />
       </p>
   </div>
 
-  <div className={style.vertical}  style={{  width: 0, height: 250, border: '0.10px black solid'}}></div>
+  <div className={styles.vertical}  style={{  width: 0, height: 250, border: '0.10px black solid'}}></div>
 
-  <div className={style.lorem1}>
-     <h1 className={style.H1}>CLIENTE</h1>
-     <p  className={style.lorems}>EASY MGT <br />
+  <div className={styles.lorem1}>
+     <h1 className={styles.H1}>CLIENTE</h1>
+     <p  className={styles.lorems}>EASY MGT <br />
        </p>
   </div> 
  
    
   </div>
 
-  <div className={style.equipo}>
+  <div className={styles.equipo}>
 
-  <h1 className={style.H1}>EQUIPO</h1>
-  <p className={style.lorems}>Mayra Muñoz
+  <h1 className={styles.H1}>EQUIPO</h1>
+  <p className={styles.lorems}>Mayra Muñoz
 
  <br />
      Ramiro Manzanos</p>
 
-     <div className={style.contEquipo}>
-      <img className={style.contEquipo} src={img1} alt="" />
+     <div className={styles.contEquipo}>
+      <img className={styles.contEquipo} src={img1} alt="" />
      </div>
 
      
-      <div className={style.videoC}>
-  <video
-    className="video"
-    autoPlay
-    loop
-    muted
-    playsInline
-  >
-    <source src="/videos/mi-video.mp4" type="video/mp4" />
-    Tu navegador no soporta video.
-  </video>
+      <div >
+  <video width="1200" height="450"  controls>
+      <source src={video} type="video/mp4" />
+      Tu navegador no soporta video HTML5.
+    </video>
 </div>
 
     
@@ -117,17 +111,17 @@ const EasyMgt = () => {
 
  
 
-<div className={style.redesEasy}>
+<div className={styles.redesEasy}>
   <RedesEasy/>
 </div>
 
- <div className={style.con}>
-            <a className={style.btn} href="#"><img className={style.botoflecha}  src={botoflecha} alt="" /></a>
+ <div className={styles.con}>
+            <a className={styles.btn} href="#"><img className={styles.botoflecha}  src={botoflecha} alt="" /></a>
                 </div>
                 
-        <div className={style.copyright}>
+        <div className={styles.copyright}>
 
-            <p className={style.copy}>Copyright © 2023 | Okey Website&Creativity | Todos los derechos reservados </p>
+            <p className={styles.copy}>Copyright © 2023 | Okey Website&Creativity | Todos los derechos reservados </p>
        
                 </div>
             
