c169a95ecfcdf63e37ebacd0fbee5ddf741e74be
yorliana <ing.yorlianam24@gmail.com>
2026-02-18 17:55:12 +0100

    cliente actualizado

--- src/components/Cliente/Cliente.jsx
+++ src/components/Cliente/Cliente.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import style from  './Cliente.module.css'
-import {Link} from "react-router-dom";
 import img from "../../assets/img/okeylogo.png"
-import flecha from '../../assets/img/flecha.png'
 import {useLocation} from "react-router-dom"
 import botoflecha from "../../assets/img/blotoflecha.png"
-import icon from "../../assets/img/flechaCorrecta4.png"
+import img23 from "../../assets/img/Trabajo23.jpg"
+import img24 from "../../assets/img/Trabajo24.jpg"
+import img25 from "../../assets/img/Trabajo25.jpg"
+import img26 from "../../assets/img/Trabajo26.jpg"
+import img27 from "../../assets/img/Trabajo27.jpg"
+import img28 from "../../assets/img/Trabajo28.jpg"
 
 const Cliente = () => {
  
@@ -38,24 +41,24 @@ de publicidad se asocian con nosotros para aprovechar nuestra experiencia en dis
 <div className={style.cards}>
 
     <div className={style.card}>
+      <img className={style.card1} src={img23} alt="" />
        
 
     </div>
 
     <div className={style.card}>
        
-
+ <img className={style.card1} src={img24} alt="" />
+       
     </div>
 
     <div className={style.card}>
+        <img className={style.card1} src={img25} alt="" />
        
 
     </div>
 
-    <div className={style.card}>
-       
-
-    </div>
+    
 
     
 
@@ -63,32 +66,35 @@ de publicidad se asocian con nosotros para aprovechar nuestra experiencia en dis
 </div>
 
 
-       
-         
-        
-       
-        
+                      
+<div className={style.cards1}>
 
+    <div className={style.card}>
+      <img className={style.card1} src={img26} alt="" />
+       
+
+    </div>
+
+    <div className={style.card}>
+       
+ <img className={style.card1} src={img27} alt="" />
+       
+    </div>
+
+    <div className={style.card}>
+        <img className={style.card1} src={img28} alt="" />
+       
+
+    </div>
+
+    
+
+    
+
+
+</div>
 
         </div>
-
-       
-     
-   
-        
-
-       
-
-
-       
-
-   
-        
-
-
-
-  
-       
 
 
   );
